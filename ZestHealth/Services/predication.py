950fb72043042b7b79fea6d0292978a811446788
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2024-11-03 15:30:14 +0530

    Off Django Import Semantics

--- ZestHealth/Services/predication.py
+++ ZestHealth/Services/predication.py
@@ -1,8 +1,8 @@
 import joblib
 from ZestHealth.nlp_model_training_utils.Classifier import classify_input
 
-model = joblib.load("./trained_model/model.pkl")
-vectorizer = joblib.load("./trained_model/vectorizer.pkl")
+model = joblib.load("./trained_nlp_model/model.pkl")
+vectorizer = joblib.load("./trained_nlp_model/vectorizer.pkl")
 
 
 def prediction(query):
